14d9ff8225b324aaa5e3f0653c8fbe02b41c56b4
Guilherme Meneghin de Souza <guilherme.meneghinsouza@gmail.com>
2025-04-24 19:04:31 -0300

    Documentando e reescrevendo o exercício da torre de Hanoi.

--- src/hanoi.c
+++ src/hanoi.c
@@ -1,85 +1,160 @@
+/****************************************************************
+ *             Resolução do Problema da Torre de Hanoi          *
+ *                                                              *
+ * Aluno: Guilherme Meneghin de Souza                           *
+ *                                                              *
+ ****************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
 
+/*
+Define uma constante para o comando de clear do systema.
+Eu optei por definir a constante, pois esse comando muda em cada sistema operacional.
+Defina para "clear" se estiver usando linux ou mac.
+*/
 #define CLEAR "CLS"
 
+// Faz o mesmo que o fflush(stdin).
+void limpar_stdin();
+
+// Pausa o programa e limpa a tela para o próximo movimento na torre.
+void pause_and_clear();
+
+// Imprime na tela o estado de cada vetor.
+void print_tower(unsigned int size);
+
+// Resolve a torre de Hanoi de forma recursiva:
+void hanoi(int *v_init, int *v_auxi, int *v_final, unsigned int size, unsigned int n);
+
+// Define na memoria o ponteiro para cada vetor do exercício.
 int *vetor_A, *vetor_T, *vetor_B;
-
-void pause_and_clear() {
-    printf("\nPress Enter to Coninue...\n");
-    getchar();
-    system(CLEAR);
-}
-
-void print_tower(unsigned int n) {
-    printf("\nTorre De Hanoi\nVa: ");
-
-    for (int i = 1; i <= n; i++) {
-        if (vetor_A[n-i] == 0) continue;
-        printf("%d, ", vetor_A[n-i]);
-    }
-
-    printf("\b\b  \nVt: ");
-
-    for (int i = 1; i <= n; i++) {
-        if (vetor_T[n-i] == 0) continue;
-        printf("%d, ", vetor_T[n-i]);
-    }
-
-    printf("\b\b  \nVb: ");
-
-    for (int i = 1; i <= n; i++) {
-        if (vetor_B[n-i] == 0) continue;
-        printf("%d, ", vetor_B[n-i]);
-    }
-
-    printf("\b\b  \n");
-}
-
-void hanoi(int* va, int* vt, int* vb, unsigned int size, unsigned int n) {
-    if (n == 1) {
-        vb[n-1] = va[n-1];
-        va[n-1] = 0;
-        print_tower(size);
-        pause_and_clear();
-    }
-    else {
-        hanoi(va, vb, vt, size, n-1);
-
-        vb[n-1] = va[n-1];
-        va[n-1] = 0;
-        print_tower(size);
-        pause_and_clear();
-
-        hanoi(vt, va, vb, size, n-1);
-    }
-}
 
 int main(int argc, char const *argv[])
 {
+    // Permite usar caracteres da lingua portuguesa e outros.
     setlocale(LC_ALL, ".utf8");
 
+    // Pergunta o número de discos da torre
     printf("Digite o tamanho do vetor inicial: ");
+
+    // Guarda o valor digitado na variável size (tamanho).
     unsigned int size;
     scanf("%u", &size);
 
-    vetor_A = malloc(size * sizeof(int));
-    vetor_T = calloc(size, sizeof(int));
-    vetor_B = calloc(size, sizeof(int));
+    // Inicializa os vetores alocando memória de acordo com o tamanho da torre.
+    vetor_A = malloc(size * sizeof(int)); // aloca memória sem limpar
+    vetor_T = calloc(size, sizeof(int));  // aloca memória, mas limpa antes.
+    vetor_B = calloc(size, sizeof(int));  // aloca memória, mas limpa antes.
 
+    // Popula a primeira torre com os N discos o menor no começo e o maior no fim.
     for (int i = 0; i < size; i++)
-        vetor_A[i] = i+1;
+        vetor_A[i] = size - i;
 
+    // Limpa a tela
     system(CLEAR);
-    getchar();
+
+    // Equivalente ao fflush().
+    // Estou fazendo desse jeito, porque fflush nao funciona no meu compilador.
+    limpar_stdin();
+
+    // Mostra o estado inicial da torre.
     print_tower(size);
     pause_and_clear();
 
+    // Resolve a torre imprimindo cada passo.
     hanoi(vetor_A, vetor_T, vetor_B, size, size);
 
+    // Libera a memória ocupada pelos vetores.
     free(vetor_A);
     free(vetor_B);
     free(vetor_T);
+    
     return 0;
+}
+
+void limpar_stdin() {
+    // A função fflush não funciona no meu compilador
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) {
+        // Lê e descarta cada caractere
+    }
+}
+
+void pause_and_clear()
+{
+    printf("\nPress Enter to Coninue...\n");
+    // Não faz nada enquanto não receber um '\n' no stdin.
+    while (getchar() != '\n')
+        ;
+    // Limpa o terminal
+    system(CLEAR);
+}
+
+void print_tower(unsigned int size)
+{
+    // Imprime Va
+    printf("\nTorre De Hanoi\nVa: ");
+    for (int i = 0; i < size; i++)
+    {
+        if (vetor_A[size - i] == 0)
+            continue;               // pula espaços vazios
+        printf("%d, ", vetor_A[i]); // imprime os números
+    }
+
+    // Imprime Vt
+    printf("\b\b  \nVt: ");
+    for (int i = 0; i < size; i++)
+    {
+        if (vetor_T[size - i] == 0)
+            continue;
+        printf("%d, ", vetor_T[i]);
+    }
+
+    // Imprime Vb
+    printf("\b\b  \nVb: ");
+    for (int i = 0; i < size; i++)
+    {
+        if (vetor_B[size - i] == 0)
+            continue;
+        printf("%d, ", vetor_B[i]);
+    }
+    printf("\b\b  \n");
+    /*
+     * Nota, '\b' move o cursor um caracter para trás, logo '\b ' exclui o último caracter.
+     * Isso serve para apagar o ', ' deixado após o último número impresso.
+     */
+}
+
+void hanoi(int *v_init, int *v_auxi, int *v_final, unsigned int size, unsigned int n)
+{
+    if (n == 1)
+    {
+        // Caso base:
+
+        // Move o primeiro disco da torre incial para a torre final.
+        v_final[size - 1] = v_init[size - 1];
+        v_init[size - 1] = 0;
+        // Mostra o estado da torre.
+        print_tower(size);
+        pause_and_clear();
+    }
+    else
+    {
+        // Caso n > 1:
+
+        // Move os n-1 primeiros discos da torre inicial para a torre auxiliar.
+        hanoi(v_init, v_final, v_auxi, size, n - 1);
+
+        // Caso base, move o último disco da torre incial para a torre final.
+        v_final[0] = v_init[0];
+        v_init[0] = 0;
+        // Mostra o estado da torre.
+        print_tower(size);
+        pause_and_clear();
+
+        // Move os n-1 primeiros discos da torre auxiliar para a torre final.
+        hanoi(v_auxi, v_init, v_final, size, n - 1);
+    }
 }
