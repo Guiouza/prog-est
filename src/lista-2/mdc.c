a3268d239996047bf6808bdf24c04474c555590a
Guilherme Meneghin de Souza <guilherme.meneghinsouza@gmail.com>
2025-05-24 21:49:29 -0300

    feat: Resolvi o exercício 4 da lista 2 (mdc)
    new file: `src/lista-2/mdc.c`

---
+++ src/lista-2/mdc.c
@@ -0,0 +1,23 @@
+#include <locale.h>
+#include <stdio.h>
+
+int mdc(int a, int b) {
+    unsigned int resto = a % b;
+    if (resto == 0)
+        return b;
+    mdc(b, resto);
+}
+
+int main(int argc, char const *argv[])
+{
+    setlocale(LC_ALL, ".utf8");
+
+    unsigned int a, b;
+    printf("Digite dois valores inteiros positivos.\na: ");
+    scanf("%u", &a);
+    printf("b: ");
+    scanf("%u", &b);
+
+    printf("MDC(a, b) = MDC(%u, %u) = %u.\n", a, b, mdc(a, b));
+    return 0;
+}
